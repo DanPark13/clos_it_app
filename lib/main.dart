98ddb5c3cf79b30128999bb8708e58dd50810625
Daniel Park <dpark0703@gmail.com>
2020-07-10 22:23:24 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(clos_it());
+}
+
+/// This Widget is the main application widget.
+class clos_it extends StatelessWidget {
+  static const String _title = 'Clos.it';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+
+  final List<Widget> _widgetOptions = <Widget>[
+    Column(
+      children: <Widget>[
+        Text(
+          "MyCloset",
+        ),
+      ],
+    ),
+    Column(
+      children: <Widget>[
+        Text(
+          "Customize",
+        )
+      ],
+    ),
+    Column(
+      children: <Widget>[
+        Text(
+          "Statistics",
+        ),
+      ],
+    ),
+    Column(
+      children: <Widget>[
+        Text(
+          "Donation",
+        )
+      ],
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Clos.it App'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.folder_shared),
+            title: Text('MyCloset'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assistant),
+            title: Text('Customize'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.equalizer),
+            title: Text('Statistics'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money),
+            title: Text('Donation'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
